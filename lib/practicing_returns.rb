d73c20d4123738ada64bc69d27c4228c5005ab9a
Angelica Cruz <brantleyhomebuyer@gmail.com>
2020-04-25 20:16:59 +0000

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,6 +3,7 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length
+  binding.pry
     yield(array[i])
     i += 1
   end
@@ -10,3 +11,23 @@ end
 
 
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+
+
+
+
+
+
+
+
+# require 'pry'
+
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+# end
+
+# binding.pry
+# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
